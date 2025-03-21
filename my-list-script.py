5e4c3b0a48228f001cea01c20a0449201bd28d5e
Cynthia-M-M <cynthiamogaka49@gmail.com>
2025-03-21 13:58:06 +0300

    Add my-list-script.py

---
+++ my-list-script.py
@@ -0,0 +1,25 @@
+# To create an empty list
+my_list = []
+
+# To append values to my list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# To insert 15 at the second position of my list
+my_list.insert(1, 15)
+
+# To extend my list with another list of values
+my_list.extend([50, 60, 70])
+
+# To remove last element in my list
+my_list.pop()
+
+# To sort my list in ascending order
+my_list.sort()
+
+# To find and print index of 30 that is in my list
+index_of_30 = my_list.index(30)
+print(my_list)
+print("Index of 30:", index_of_30)
